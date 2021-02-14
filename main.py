53cf66516da2004985f572af2f729cf5d0e47c5c
Jean MacBride <Jamin4me@gmail.com>
2021-02-14 15:09:02 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+root.geometry ('300x300')
+root.title ('Python Mad Libs Generator')
+Label (root, text = 'Mad Libs Generator, enjoy!', font= 'helvetica 15 bold') .pack()
+Label (root, text = 'Click One!'font= 'helvetica 15 bold') .place (x=40, y=80)
