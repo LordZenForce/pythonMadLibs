a6b21ef2fe70828892d71f360b26f368298cf010
LordZenForce <Jamin4me@gmail.com>
2021-02-14 16:17:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from tkinter import * 
 
-root = TK()
+root = Tk()
 root.geometry ('300x300')
 root.title ('Python Mad Libs Generator')
-Label (root, text = 'Mad Libs Generator, enjoy!', font= 'arial 15 bold') .pack()
+Label (root, text = 'Mad Libs Generator, enjoy!' , font= 'arial 15 bold').pack()
 Label (root, text = 'Click One!', font= 'arial 15 bold') .place (x=40, y=80)
 
 def madlib():
@@ -15,10 +15,15 @@ def madlib():
   place= input ('enter a place: ')
   verb= input ('enter a verb: ')
   food= input ('enter the name of a food: ')
-  
   print( 'Oh wow, its a ' + animal + '! I cant believe I was foolish enough to leave out my ' + pottery + '. The ' + profession + ' said before yelling ' + name + ' go to ' + place + ' and ' + verb + ' so we can chase the animal away with ' + food + '.')
   
+<<<<<<< Updated upstream
   Button (root, text= 'A Tragedy!', font= 'Helvetica 10', command= madlib(), bg= 'ghost white').place (x=80, y=240)
   
   root.mainloop()
+=======
+  Button(root, text= 'A Tragedy!', font= 'arial 10', command= madlib(), bg = 'ghost white').place(x=60, y=120)
+
+root.mainloop()
+>>>>>>> Stashed changes
   
