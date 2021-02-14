2697e987883a911810bee41e16fea8af0df3cdfd
Jean MacBride <Jamin4me@gmail.com>
2021-02-14 17:14:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ def madlib1():
   place= input ('enter a place: ')
   verb= input ('enter a verb: ')
   food= input ('enter the name of a food: ')
-  print( 'Oh wow, its a ' + animal + '! I cant believe I was foolish enough to leave out my ' + pottery + '. The ' + profession + ' said before yelling ' + name + ' go to ' + place + ' and ' + verb + ' so we can chase the animal away with ' + food + '.')
+  print(' Oh wow, its a ' + animal + '! I cant believe I was foolish enough to leave out my ' + pottery + '. The ' + profession + ' said before yelling ' + name + ' go to ' + place + ' and ' + verb + ' so we can chase the animal away with ' + food + '.')
   
-  Button(root, text= 'A Tragedy!', font= 'arial 10', command= madlib1, bg= 'ghost white').place(x=60, y=120)
-  
+Button(root, text = 'A Tragedy!', font= 'arial 15', command=madlib1, bg= 'ghost white').place(x=60, y=120)
+
 root.mainloop()
+
 
